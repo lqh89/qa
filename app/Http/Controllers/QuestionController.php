f105c683cbc093da4dea5cedbfb46b8d582bfbdf
DESKTOP-4S4SRS5\hungl <lequanghungit@gmail.com>
2019-05-14 13:55:13 +0700

    them delete

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -85,6 +85,7 @@ class QuestionController extends Controller
      */
     public function destroy(Question $question)
     {
-        //
+        $question->delete();
+        return redirect('/questions')->with('success','Your questions has been delete.');
     }
 }
