4790fbf119b24fc7eabde0615908e3f1434bb309
DESKTOP-4S4SRS5\hungl <lequanghungit@gmail.com>
2019-05-15 15:49:28 +0700

    tao fake answer

--- app/Answer.php
+++ app/Answer.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Answer extends Model
 {
     public function question(){
-        return $this->belongsTo(Answer::class);
+        return $this->belongsTo(Question::class);
     }
 
     public function user(){
@@ -16,5 +16,15 @@ class Answer extends Model
 
     public function getBodyHtmlAttribute(){
         return \Parsedown::instance()->text($this->body);
+    }
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::created(function ($answer){
+            $answer->question->increment('answers_count');
+            $answer->question->save();
+        });
     }
 }
